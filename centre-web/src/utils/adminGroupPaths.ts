8f4059530ca70e84756fce138c2c30112f3fb060
Dinesh <97143739+dinesh-aot@users.noreply.github.com>
2026-02-11 09:32:34 -0800

    Change EPIC_PUBLIC admin group to SUPER_ADMIN

--- centre-web/src/utils/adminGroupPaths.ts
+++ centre-web/src/utils/adminGroupPaths.ts
@@ -17,7 +17,7 @@ export const EPIC_CLIENT_TO_ADMIN_GROUP_PATHS: Record<
   [EpicAppClientName.EPIC_ENGAGE]: "ENGAGE/INSTANCE_ADMIN",
   [EpicAppClientName.EPIC_SUBMIT]: "SUBMIT/EAO_MANAGER",
   [EpicAppClientName.CONDITION_REPOSITORY]: "CONDITION-REPO/ADMIN",
-  [EpicAppClientName.EPIC_PUBLIC]: "PUBLIC/ADMIN",
+  [EpicAppClientName.EPIC_PUBLIC]: "PUBLIC/SUPER_ADMIN",
   [EpicAppClientName.DOCUMENT_SEARCH]: "", // No admin group
 };
 
